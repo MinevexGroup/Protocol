425629927dda6687ecf805dd4c105340a3a4a1ee
Kaooot <jonapapenfuss@gmail.com>
2023-04-28 14:26:24 +0200

    Update command params for 1.19.80

--- bedrock/bedrock-v582/src/main/java/com/nukkitx/protocol/bedrock/v582/BedrockPacketHelper_v582.java
+++ bedrock/bedrock-v582/src/main/java/com/nukkitx/protocol/bedrock/v582/BedrockPacketHelper_v582.java
@@ -1,6 +1,7 @@
 package com.nukkitx.protocol.bedrock.v582;
 
 import com.nukkitx.protocol.bedrock.data.LevelEventType;
+import com.nukkitx.protocol.bedrock.data.command.CommandParam;
 import com.nukkitx.protocol.bedrock.data.inventory.ContainerSlotType;
 import com.nukkitx.protocol.bedrock.v575.BedrockPacketHelper_v575;
 import lombok.AccessLevel;
@@ -14,6 +15,29 @@ import lombok.NoArgsConstructor;
 public class BedrockPacketHelper_v582 extends BedrockPacketHelper_v575 {
 
     public static final BedrockPacketHelper_v582 INSTANCE = new BedrockPacketHelper_v582();
+
+    @Override
+    protected void registerCommandParams() {
+        this.addCommandParam(1, CommandParam.INT);
+        this.addCommandParam(3, CommandParam.FLOAT);
+        this.addCommandParam(4, CommandParam.VALUE);
+        this.addCommandParam(5, CommandParam.WILDCARD_INT);
+        this.addCommandParam(6, CommandParam.OPERATOR);
+        this.addCommandParam(7, CommandParam.COMPARE_OPERATOR);
+        this.addCommandParam(8, CommandParam.TARGET);
+        this.addCommandParam(10, CommandParam.WILDCARD_TARGET);
+
+        this.addCommandParam(17, CommandParam.FILE_PATH);
+
+        this.addCommandParam(44, CommandParam.STRING);
+        this.addCommandParam(52, CommandParam.BLOCK_POSITION);
+        this.addCommandParam(53, CommandParam.POSITION);
+        this.addCommandParam(55, CommandParam.MESSAGE);
+        this.addCommandParam(58, CommandParam.TEXT);
+        this.addCommandParam(62, CommandParam.JSON);
+        this.addCommandParam(71, CommandParam.BLOCK_STATES);
+        this.addCommandParam(74, CommandParam.COMMAND);
+    }
 
     @Override
     protected void registerContainerSlotTypes() {
