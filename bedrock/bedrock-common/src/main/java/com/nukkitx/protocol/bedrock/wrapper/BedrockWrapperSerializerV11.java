9b7a9b8df23e6eceafd73abc875b664e53439859
KeksDev <54219265+tunikakeks@users.noreply.github.com>
2023-06-03 23:23:54 +0200

    Update BedrockWrapperSerializerV11.java
    testing lol

--- bedrock/bedrock-common/src/main/java/com/nukkitx/protocol/bedrock/wrapper/BedrockWrapperSerializerV11.java
+++ bedrock/bedrock-common/src/main/java/com/nukkitx/protocol/bedrock/wrapper/BedrockWrapperSerializerV11.java
@@ -53,7 +53,7 @@ public class BedrockWrapperSerializerV11 extends BedrockWrapperSerializer {
     public void deserialize(ByteBuf compressed, BedrockPacketCodec codec, Collection<BedrockPacket> packets, BedrockSession session) {
         ByteBuf decompressed = ByteBufAllocator.DEFAULT.ioBuffer();
         try {
-            this.compressionSerializer.decompress(compressed, decompressed, 12 * 1024 * 1024); // 12MBs
+            this.compressionSerializer.decompress(compressed, decompressed, 50 * 1024 * 1024); // 12MBs
 
             while (decompressed.isReadable()) {
                 int length = VarInts.readUnsignedInt(decompressed);
